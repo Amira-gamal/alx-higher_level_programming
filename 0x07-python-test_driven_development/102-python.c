3dab6392feb6ffbfd3b080288875c1b344b32b13
Amira-gamal <amiragamal7814@gmail.com>
2024-04-12 18:13:43 -0700

    jkl

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -1,18 +1,19 @@
-#include <Python.h>
 #include <stdio.h>
 #include <string.h>
+#include <Python.h>
 
 /**
- * print_python_string - Print string from python
+ * print_python_string - Prints string information
+ * 
  * @p: Python Object
- * Return: none
+ * Return: no return
  */
-
 void print_python_string(PyObject *p)
 {
-	PyObject *str, *tmp;
+	
+	PyObject *str, *repr;
 
-	(void)tmp;
+	(void)repr;
 	printf("[.] string object info\n");
 
 	if (strcmp(p->ob_type->tp_name, "str"))
@@ -26,9 +27,8 @@ void print_python_string(PyObject *p)
 	else
 		printf("  type: compact unicode object\n");
 
-	tmp = PyObject_Repr(p);
+	repr = PyObject_Repr(p);
 	str = PyUnicode_AsEncodedString(p, "utf-8", "~E~");
-
 	printf("  length: %ld\n", PyUnicode_GET_SIZE(p));
 	printf("  value: %s\n", PyBytes_AsString(str));
 }
